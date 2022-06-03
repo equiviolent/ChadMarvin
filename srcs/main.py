2f11437bd12d66bbdee5b5cb7623c4a8b000d327
Archer <leetpwnsroot@gmail.com>
2022-06-03 13:58:38 +0200

    Remove garbages

--- srcs/main.py
+++ srcs/main.py
@@ -36,16 +36,9 @@ async def on_member_join(member):
 def randomPain():
     painWords = ['why','bruh', 'pain', 'no', 'really ?','torment','misery','I am aching', 'Y u n0 u53 4rch??1','https://www.digitalcitizen.life/how-uninstall-windows-8-windows-7-or-any-other-version-windows/','Please dont post this it isnt funny. Never in all my years on this earth have I cringed at a message so much. I swear on God that seeing this word brings me a pain which I cant describe so please for the sake of my body and mind dont ever post this again or I may legitimately die from cringe']
     return ''.join(random.sample(painWords,1))
-<<<<<<< HEAD
 
 @client.event
 async def on_message(message):
-=======
-@client.event
-async def on_message(self, message):
-    if message.author == self.user:
-        return
->>>>>>> 44b35fdb0dec78276b0f0618799213bb81b4d1d6
     if 'windows' in  message.content.lower():
         await message.channel.send(randomPain(), reference=message)
 
