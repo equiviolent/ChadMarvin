a91384f8a0e884053591083caa1fc0bfa2674fa2
Archer <leetpwnsroot@gmail.com>
2022-06-03 13:50:46 +0200

    Funny cmd works

--- srcs/main.py
+++ srcs/main.py
@@ -32,19 +32,13 @@ async def on_member_join(member):
             f'Hi {member.name}, welcome to the ChadConributors server!'
             )
 
-#@client.event
-#async def on_message(message):
-#    if 'loser' in message.content.lower():
-#        await message.channel.send('Yes I agree Tanguy is loser! 🎈')
-
 #This function makes the bot seem depressed if you say windows
 def randomPain():
     painWords = ['why','bruh', 'pain', 'no', 'really ?','torment','misery','I am aching', 'Y u n0 u53 4rch??1','https://www.digitalcitizen.life/how-uninstall-windows-8-windows-7-or-any-other-version-windows/','Please dont post this it isnt funny. Never in all my years on this earth have I cringed at a message so much. I swear on God that seeing this word brings me a pain which I cant describe so please for the sake of my body and mind dont ever post this again or I may legitimately die from cringe']
     return ''.join(random.sample(painWords,1))
 
-async def on_message(self, message):
-    if message.author == self.user:
-        return
+@client.event
+async def on_message(message):
     if 'windows' in  message.content.lower():
         await message.channel.send(randomPain(), reference=message)
 
